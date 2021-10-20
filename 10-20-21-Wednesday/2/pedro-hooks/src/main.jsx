7aca86f8e2d38908ca15232406fcc741b1f7b566
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-10-20 16:12:54 -0500

    🤔 Started main content of article

--- 10-20-21-Wednesday/2/pedro-hooks/src/main.jsx
+++ 10-20-21-Wednesday/2/pedro-hooks/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import StateTutorial from "./UseState/StateTutorial";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StateTutorial />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
